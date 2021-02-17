2310f3e3d33263ab51c278494b6994d9bb0490ff
luciano cardozo casariego <lucianoc.c117@gmail.com>
2021-02-17 20:13:04 -0300

    Intentando correr en local

--- src/routes/App.js
+++ src/routes/App.js
@@ -11,7 +11,7 @@ import Player from '../containers/Player';
 
 const App = () => (
 
-    <BrowserRouter>
+    <BrowserRouter /*basename='/build'*/>
         <Layout>
             <Switch>
                 <Route exact path="/" component={Home}/>
